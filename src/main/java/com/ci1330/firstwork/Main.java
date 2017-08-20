d90121c025fd8e036ece84b3d2aae0f33dc74bcb
JosueCuberoSanchez <josue.cubero@ucr.ac.cr>
2017-08-19 23:00:48 -0600

    XML v5

--- src/main/java/com/ci1330/firstwork/Main.java
+++ src/main/java/com/ci1330/firstwork/Main.java
@@ -16,7 +16,15 @@ public class Main {
             InputStream is = new FileInputStream(file);
             Builder parser = new Builder();
             Document doc = parser.build(is);
-            printChildren(doc.getRootElement());
+            //voy a hacerlo pasito por pasito
+            Element root = doc.getRootElement(); //este saca el raiz, que es el que engloba todoo
+            Element child = root.getFirstChildElement("messages"); // este saca un hijo que tenga de tag el nombre que le paso
+            Elements children = child.getChildElements(); //este saca todos los hijos de ese elemento, que serian todos los messages
+            Element e;
+            for(int i=0;i<children.size();i++){ //aca itero por cada message
+                e = children.get(i); //saco el n-esimo hijo
+                System.out.println(e.getValue()); //imprimo el valor
+             } //si lo ve enredado, recuerde que un XML tiene forma de arbol.
             System.out.println(doc.toXML());
         } catch (Exception e) {
             System.out.println(e);
@@ -24,7 +32,7 @@ public class Main {
     }
 
     public static void printChildren(Element e){
-        System.out.println(e.getValue());
+        System.out.println(e.getFirstChildElement("").getValue());
         Elements elements = e.getChildElements();
         for(int i=0;i<elements.size();i++){
             Element child = elements.get(i);
